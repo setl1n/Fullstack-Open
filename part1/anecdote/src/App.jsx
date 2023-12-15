f5f5b4cf9e9571354f7efa8b4f012918c5011ef9
setl1n <kaungsetlin2008@gmail.com>
2023-12-15 13:02:44 +0800

    finished 1.11-1.13

---
+++ part1/anecdote/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+function getRandomInt(max) {
+  let num = Math.floor(Math.random() * max)
+  console.log(num)
+  return num;
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const incrementVote = (votes, selected, setVotes) => {
+  let cpy = [...votes]
+  cpy[selected]++
+  setVotes(cpy)
+}
+
+const getIndexOfHighest = (votes) => {
+  let highestIndex = 0
+  for (let i = 0; i < votes.length; i++) {
+    if (votes[i] > votes[highestIndex]) {
+      highestIndex = i;
+    }
+  }
+  return highestIndex
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint8Array(8))
+  console.log(votes)
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={() => incrementVote(votes, selected, setVotes)} text="vote" />
+      <Button handleClick={() => setSelected(getRandomInt(anecdotes.length))} text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[getIndexOfHighest(votes)]}</p>
+    </div>
+  )
+}
+
+export default App
