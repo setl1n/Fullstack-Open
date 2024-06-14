b2f56355f21b41579c48aebc8fa314476e0634f9
setl1n <kaungsetlin2008@gmail.com>
2024-06-14 14:07:56 +0700

    processed response from openweather api to integrate front end

--- part2/country_info/src/componenets/CountryInfo.jsx
+++ part2/country_info/src/componenets/CountryInfo.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import restcountryapi from '../services/restcountryapi';
 import weatherapi from '../services/weatherapi';
 
-const WeatherInfo = (({ lat, lng}) => {
+const WeatherInfo = (({ countryName, lat, lng}) => {
     const [weatherInfo, setWeatherInfo] = useState(null);
     
     useEffect(() => {
@@ -18,7 +18,14 @@ const WeatherInfo = (({ lat, lng}) => {
         return <></>;
     }
 
-    return <>weather info acquired!</>
+    return <>
+    <h2>
+        Weather in {countryName}
+    </h2>
+    <div>temperature {weatherInfo.current.temp} Celcius</div>
+    <img src={`https://openweathermap.org/img/wn/${weatherInfo.current.weather[0].icon}@2x.png`}></img>
+    <div>wind {weatherInfo.current.wind_speed} </div>
+    </>
 })
 
 const SpecificCountryInfo = (({ countryName }) => {
@@ -44,7 +51,7 @@ const SpecificCountryInfo = (({ countryName }) => {
         <h2>languages:</h2>
         <ul>{Object.values(countryInfo.languages).map((language, index) => <li key={index}>{language}</li>)}</ul>
         <img src={countryInfo.flags.png} alt="Picture of Flag"></img>
-        < WeatherInfo lat={countryInfo.latlng[0]} lng={countryInfo.latlng[1]}/>
+        < WeatherInfo countryName={countryInfo.name.common}lat={countryInfo.latlng[0]} lng={countryInfo.latlng[1]}/>
     </>;
 })
 
--- part2/country_info/src/services/weatherapi.js
+++ part2/country_info/src/services/weatherapi.js
@@ -3,7 +3,7 @@ const baseurl = "https://api.openweathermap.org/data/3.0/onecall?";
 const api_key = import.meta.env.VITE_api_key;
 
 const getWeatherInfo = (async (lat , lng) => {
-    let response = await axios.get(`${baseurl}lat=${lat}&lon=${lng}&appid=${api_key}`);
+    let response = await axios.get(`${baseurl}lat=${lat}&lon=${lng}&appid=${api_key}&units=metric`);
     return response.data;
 })
 
