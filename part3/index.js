682b2797ce583d32047bb9a68395410bccf87f78
setl1n <kaungsetlin2008@gmail.com>
2024-12-09 19:24:07 +0800

    part3.16: implemented catch for casterror to invalid ids

--- part3/index.js
+++ part3/index.js
@@ -84,6 +84,10 @@ app.post('/api/persons', (req, res) => {
 
 const errorHandler = (err, req, res, next) => {
     console.log(err);
+
+    if (err.name == "CastError") {
+        return res.status(400).send({error: "malformatted id"});
+    }
     next(err)
 }
 
