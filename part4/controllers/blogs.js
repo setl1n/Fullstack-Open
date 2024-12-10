d0c8adbb0aeaec6f98f41584943464bae94d8196
setl1n <kaungsetlin2008@gmail.com>
2024-12-10 17:35:15 +0800

    part4.8: integration test initialisation, database mocking and get endpoint test

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,17 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
+const logger = require('../utils/logger')
 
-blogsRouter.get('', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogsRouter.post('', (request, response) => {
   const blog = new Blog(request.body)
-  console.log(request.body)
+  logger.info(request.body)
   blog
     .save()
     .then(result => {
---
+++ part4/tests/blogs_api.test.js
@@ -0,0 +1,30 @@
+const { test, after , beforeEach } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/Blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let newBlog = new Blog(blog)
+    await newBlog.save()
+  }
+  console.log('all blogs from test file loaded onto mongoDB')
+})
+
+test('notes are returned as json', async () => {
+  const res = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  assert.equal(res.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
