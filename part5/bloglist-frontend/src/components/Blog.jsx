10d9001f11da75e8b468b057cd75ddd125b46876
setl1n <kaungsetlin2008@gmail.com>
2024-12-12 19:08:59 +0800

    part5.7: hide and view blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle} >
+      <div>
+        {blog.title}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+        <button style={showWhenVisible}onClick={toggleVisibility}>hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.author}
+        <br/>
+        {blog.url}
+        <br/>
+        likes {blog.likes} <button>like</button>
+        <br/>
+        {blog.user.name}
+      </div>
+    </div >
+  )
+}
 
 export default Blog
