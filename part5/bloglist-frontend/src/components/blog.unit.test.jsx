b050ed83fd7d0c73183edb3a6f685a1e6746b7c6
setl1n <kaungsetlin2008@gmail.com>
2024-12-12 23:43:58 +0800

    part5.15: like is clicked twice triggers event handler twice

--- part5/bloglist-frontend/src/components/blog.unit.test.jsx
+++ part5/bloglist-frontend/src/components/blog.unit.test.jsx
@@ -14,33 +14,44 @@ let sampleBlog = {
   },
   'id': '67586e65494b2c6118ec05a4'
 }
-let container
-
-beforeEach(() => {
-  container = render(
-    <Blog blog={sampleBlog}/>
-  ).container
-})
 
 describe('rendering blogs by default', () => {
   test('renders the blog\'s title', () => {
+    render(<Blog blog={sampleBlog}/>)
     const renderedTitle = screen.getByText('My christmas story', { exact: false })
     expect(renderedTitle).toBeDefined()
   })
   test('renders the blog\'s author', async () => {
+    render(<Blog blog={sampleBlog}/>)
     const renderedAuthor = screen.getByText('Santa Claus', { exact: false })
     expect(renderedAuthor).toBeDefined()
   })
   test('does not render the blog\'s likes and URL', () => {
+    let container = render(
+      <Blog blog={sampleBlog}/>
+    ).container
     const hiddenLikes = container.querySelector('.likesAndUrl')
     expect(hiddenLikes).toHaveStyle('display: none')
   })
 })
 
 test('clicking view shows URL and likes', async () => {
+  let container = render(
+    <Blog blog={sampleBlog}/>
+  ).container
   const user = userEvent.setup()
   const showButton = screen.getByText('view')
   await user.click(showButton)
   const likesAndUrlDiv = container.querySelector('.likesAndUrl')
   expect(likesAndUrlDiv).not.toHaveStyle('display: none')
+})
+
+test('clicking like twice triggers event handler twice', async () => {
+  const user = userEvent.setup()
+  let likeBlogMock = vi.fn()
+  render(<Blog blog={sampleBlog} likeBlog={likeBlogMock}/>)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+  expect(likeBlogMock.mock.calls).toHaveLength(2)
 })
