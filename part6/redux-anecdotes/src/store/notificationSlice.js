339ef1f6b3709a43ef98ed78206830ffa5a0b877
setl1n <kaungsetlin2008@gmail.com>
2024-12-13 20:47:51 +0800

    part6.12: notification slice

---
+++ part6/redux-anecdotes/src/store/notificationSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = 'Initial Notification'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducer: {
+
+  }
+})
+
+export default notificationSlice.reducer
--- part6/redux-anecdotes/src/store/store.js
+++ part6/redux-anecdotes/src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import anecdoteReducer from "./anecdotesSlice";
 import filterReducer from "./filterSlice";
+import notificationReducer from "./notificationSlice";
 
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 })
 
