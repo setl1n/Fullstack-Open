93ae79e7a092ba91016601c5ae4a3f29231e0c8f
setl1n <kaungsetlin2008@gmail.com>
2024-12-13 17:11:12 +0800

    refactor: combined use combined reducers

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,7 @@ import { vote } from '../reducers/anecdoteReducer'
 
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
   const dispatch = useDispatch()
 
   const voteId = (id) => {
