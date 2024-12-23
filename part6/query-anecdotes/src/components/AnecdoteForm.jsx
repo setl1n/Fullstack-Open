3112aa7cba1cb09f97e2228701adf6a263814bc9
setl1n <kaungsetlin2008@gmail.com>
2024-12-23 13:17:06 +0800

    part6.21: feature: create new anecdotes using react query library

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,8 +1,21 @@
+import { createAnecdote } from '../requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+ })
+  const getId = () => (100000 * Math.random()).toFixed(0)
+
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    newAnecdoteMutation.mutate({content, id: getId(), votes: 0})
     event.target.anecdote.value = ''
     console.log('new anecdote')
 }
