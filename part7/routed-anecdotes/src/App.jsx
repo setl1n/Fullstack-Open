f3ee862603c1e578bc139c6c78f0e5f8bd62da97
setl1n <kaungsetlin2008@gmail.com>
2024-12-23 14:52:48 +0800

    part7.1: fake pages using react router

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import {
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +9,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <a href='/' style={padding}>anecdotes</a>
+      <a href='/create' style={padding}>create new</a>
+      <a href='/about' style={padding}>about</a>
     </div>
   )
 }
@@ -126,9 +129,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />}/>
+        <Route path="/create" element={<CreateNew addNew={addNew} />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
       <Footer />
     </div>
   )
